6f95551cd75936ebe22bd73c14f0e139ca676d3f
Makarios Chung <m.zhong1598@gmail.com>
2016-10-20 00:43:46 -0400

    Added functionality on Thursdays (Wednesdays?)

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,22 @@
 
+$(document).ready(function(){
+
+var d = new Date();
+var m = d.getMinutes();
+var t = d.getHours();
+var w = d.getDay();
+
+console.log(w);
+console.log(t);
+console.log(m);
+
+t = t+(m/60);
+
 function closeall(){
 
-	$(document).ready(function(){
-    	$('.open').toggleClass('open closed');
-    	$(".status").html("CLOSED");
-	});
+    $('.open').toggleClass('open closed');
+    $(".status").html("CLOSED");
+
 //	var elements = document.querySelectorAll(".open");
 //	var status = document.getElementsByClassName(".status");
 //	$(".open").removeClass("open").addClass("closed");
@@ -16,10 +28,10 @@ function closeall(){
 
 function openall(){
 
-	$(document).ready(function(){
-		$(".closed").toggleClass('closed open');
-		$(".status").html("OPEN");
-	})
+
+	$(".closed").toggleClass('closed open');
+	$(".status").html("OPEN");
+
 //	var elements = document.querySelectorAll(".closed");
 //	var status = document.querySelectorAll(".status");
 //	for(i = 0; i < elements.length; i++){
@@ -29,10 +41,56 @@ function openall(){
 //	}
 }
 
-function openSpecific(id){
-
-	$(document).ready(function(){
+function openOne(id){
+	if ($("#"+id).hasClass('closed')){
 		$("#"+id).toggleClass('closed open');
 		$("#"+id+" .status").html("OPEN");
-	})
+	}
 }
+
+function closeOne(id){
+	if ($("#"+id).hasClass('open')){
+		$("#"+id).toggleClass('open closed');
+		$("#"+id+" .status").html("CLOSED");
+	}	
+}
+
+window.onload = function () {
+
+	switch(w){
+		case 0: switch (true){
+					case (t < 11):
+						 break;
+					case (t > 11 && x < 3):
+						 openOne('nch');
+						 openOne('commons');
+					case (t > 12 && x < 19):
+						 openOne('houston');
+					case (t > 17 && x < 20):
+						 openOne('nch');
+						 openOne('commons');
+					}
+		case 4: switch (true){
+					case (t < 7.5):
+						break;
+					case (t >= 7.5 && t < 10):
+						 openOne('nch');
+					case (t >= 8.5 && t < 10):
+						 openOne('kceh');
+					case (t >= 11 && t < 19):
+						 openOne('houston')
+					case (t >= 11 && t < 14):
+						 openOne('nch');
+						 openOne('kceh');
+						 openOne('commons');
+					case (t >= 17 && t < 20):
+						 openOne('nch');
+					case (t >= 17 && t < 21):
+						 openOne('kceh');
+						 openOne('commons');
+					}
+			}
+		} 
+
+
+});
