1ea261fa28f1898a7b67c6860892f101fe82cec5
Makarios Chung <m.zhong1598@gmail.com>
2016-10-21 17:16:15 -0400

    Added inRange function; full week schedule

--- scripts/main.js
+++ scripts/main.js
@@ -34,6 +34,7 @@ function openOne(id){
 	if ($("#"+id).hasClass('closed')){
 		$("#"+id).toggleClass('closed open');
 		$("#"+id+" .status").html("OPEN");
+		console.log(id+' has opened')
 	}
 }
 
@@ -44,6 +45,88 @@ function closeOne(id){
 	}	
 }
 
+function inRange(x, min, max){
+	return (x > min && x < max);
+}
+
+function fridayTimes(t){
+	if (inRange(t, 7.5, 10)){
+		openOne('nch');
+	}
+	if (inRange(t, 8.5, 10.5)){
+		openOne('kceh');
+	}
+	if (inRange(t, 11, 17)){
+		openOne('houston');
+	}
+	if (inRange(t, 11, 14)){
+		openOne('nch');
+		openOne('kceh');
+		openOne('commons');
+	}
+	if (inRange(t, 17, 19)){
+		openOne('nch');
+	}
+	if (inRange(t, 17, 19.5)){
+		openOne('commons');
+	}
+	if (inRange(t, 17, 21)){
+		openOne('kceh');
+	}
+}
+
+function saturdayTimes(t){
+	if (inRange(t, 11, 15)){
+		openOne('nch');
+		openOne('commons');
+	}
+	if (inRange(t, 17, 19)){
+		openOne('nch');
+	}
+	if (inRange(t, 17, 19.5)){
+		openOne('commons');
+	}
+}
+
+function sundayTimes(t){
+	if (inRange(t, 11, 15)){
+		openOne('nch');
+		openOne('commons');
+	}
+	if (inRange(t, 12, 19)){
+		openOne('houston');
+	}
+	if (inRange(t, 17, 20)){
+		openOne('nch');
+		openOne('commons');
+	}
+}
+
+function weekdayTimes(t){
+	if (inRange(t, 7.5, 10)){
+		openOne('nch');
+	}
+	if (inRange(t, 8.5, 10.5)){
+		openOne('kceh');
+	}
+	if (inRange(t, 11, 19)){
+		openOne('houston');
+	}
+	if (inRange(t, 11, 14)){
+		openOne('nch');
+		openOne('kceh');
+		openOne('commons');
+	}
+	if (inRange(t, 17, 20)){
+		openOne('nch');
+	}
+	if (inRange(t, 17, 21)){
+		openOne('kceh');
+		openOne('commons');
+	}
+}
+
+
 window.onload = function () {
 
 	var d = new Date();
@@ -51,48 +134,25 @@ window.onload = function () {
     var t = d.getHours();
     var w = d.getDay();
 
-    console.log(w);
+	console.log (inRange(3.877, 2, 5));
+    console.log('w'+w);
     console.log(t);
     console.log(m);
 
 	t = t+(m/60);
 
+	console.log(t);
 
-	switch(true){
-		case (w == 0): switch (true){
-					case (t < 11):
-						 break;
-					case (t > 11 && x < 3):
-						 openOne('nch');
-						 openOne('commons');
-					case (t > 12 && x < 19):
-						 openOne('houston');
-					case (t > 17 && x < 20):
-						 openOne('nch');
-						 openOne('commons');
-					}
-		case (w >= 1 && w <= 4): switch (true) {
-					case (t < 7.5):
-						break;
-					case (t >= 7.5 && t < 10):
-						 openOne('nch');
-					case (t >= 8.5 && t < 10.5):
-						 openOne('kceh');
-						 break;
-					case (t >= 11 && t < 19):
-						 openOne('houston')
-					case (t >= 11 && t < 14):
-						 openOne('nch');
-						 openOne('kceh');
-						 openOne('commons');
-					case (t >= 17 && t < 20):
-						 openOne('nch');
-					case (t >= 17 && t < 21):
-						 openOne('kceh');
-						 openOne('commons');
-					}
-			}
-		} 
+	switch(w){
+		case 0 : sundayTimes(t);
+		case 1 : weekdayTimes(t);
+		case 2 : weekdayTimes(t);
+		case 3 : weekdayTimes(t);
+		case 4 : weekdayTimes(t);
+		case 5 : fridayTimes(t);
+		case 6 : saturdayTimes(t);
+		}
+	} 
 
 
 });
