9c72c1eb20cd8a6c902d31fe082d6b774693e6d6
Makarios Chung <m.zhong1598@gmail.com>
2017-01-06 02:11:08 -0500

    Daily Menu Update

--- scripts/dinetrackgetmenu.sh
+++ scripts/dinetrackgetmenu.sh
@@ -3,4 +3,4 @@ cd ~/Documents/DineTrack
 python scripts/getMenu.py
 git add .
 git commit -m "Daily Menu Update"
-git push origin master
+git push https://notsosmartasian:kayish12@github.com/notsosmartasian/notsosmartasian.github.io.git master
