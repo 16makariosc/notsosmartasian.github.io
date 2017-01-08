21291901f864ba611fa9239dca0ba01203df5a0b
Makarios Chung <m.zhong1598@gmail.com>
2017-01-08 18:12:08 -0500

    menu testing again

---
+++ index.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"
+  			integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+  			crossorigin="anonymous"></script>
+  <script src="scripts/main.js"></script>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-86316476-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+	<title>Penn DineTime</title>
+	<meta name  = "author" content = "Makarios Chung"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+</head>
+
+<body>
+
+	<div id = "banner">Site may (will certainly) not be accurate during break. Happy Holidays!</div>
+	<ul>
+
+		<li id = "1920-commons" class = "closed"><button>Commons<span class = "status">CLOSED</span></button></li>
+
+			<ul id = "1920-commonsmenu" class = "hidden menu">										
+			</ul>
+
+
+		<li id = "kings-court-english-house" class = "closed"><button>KCEH<span class = "status">CLOSED</span></button></li>
+			<ul id = "kings-court-english-housemenu" class = "hidden menu">										
+			</ul>
+
+
+
+		<li id = "mcclelland" class = "closed"><button>McClelland<span class = "status">CLOSED</span></button></li>
+			<ul id = "mcclellandmenu" class = "hidden menu">										
+			</ul>
+
+
+
+		<li id = "new-college-house" class = "closed"><button>NCH<span class = "status">CLOSED</span></button></li>
+			<ul id = "new-college-housemenu" class = "hidden menu">
+			</ul>
+
+		<li id = "new-college-houseretail" class = "closed"><button>NCH&nbspRetail<span class = "status">CLOSED</span></button></li>
+
+		<li id = "new-college-housemealeq" class = "closed"><button>NCH&nbspMeal&nbspEquivalency<span class = "status">CLOSED</span></button></li>
+	
+		<li id = "houston-market" class = "closed"><button>Houston<span class = "status">CLOSED</span></button></li>
+			<ul id = "houston-marketmenu" class = "hidden">
+			</ul>
+
+
+
+		<li id = "frontera" class = "closed"><button>Frontera<span class = "status">CLOSED</span></button></li>
+			<ul id = "fronteramenu" class = "hidden">
+			</ul>
+
+
+
+		<li id = "accenture" class = "closed"><button>Accenture<span class = "status">CLOSED</span></button></li>
+			<ul id = "accenturemenu" class = "hidden">
+			</ul>
+
+	</ul>
+</body>
+
+<footer>
+  <p id = "footer"> DineTime 0.2. by Makarios Chung. Send Questions, Feedback, and Hatemail to <a href="mailto:chungma@seas.upenn.edu">chungma@seas.upenn.edu</a>.</p>
+
+</html>
+
+
+
+<!-- Debugging/JS Testing Buttons 
+<button onclick="openOne('commons')">OpenCommons</button> 
+<button onclick="openOne('kceh')">OpenKCEH</button> 
+
+<button onclick="closeall()">Close</button><br>
+<button onclick="openall()">Open</button>  -->
