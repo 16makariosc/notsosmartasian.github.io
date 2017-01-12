989c02f5a3f719813142d1c8626a8843cab2f56d
notsosmartasian <m.zhong1598@gmail.com>
2017-01-12 00:03:26 -0500

    Delete dinetrackgetmenu.sh

--- scripts/dinetrackgetmenu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/Documents/DineTrack
-python scripts/getMenu.py
-git add .
-git commit -m "Daily Menu Update"
-git push https://notsosmartasian:kayish12@github.com/notsosmartasian/notsosmartasian.github.io.git master
